e1767e0d0cb8eeed25e0f3239b59431133058048
huanghuzz <2624190188@qq.com>
2024-10-23 17:32:13 +0800

    1

--- 1.py
+++ 1.py
@@ -1,26 +1,15 @@
-#import torch
-
-#print(torch.cuda.is_available())
+'''检查环境是否可以调用cuda和一些安装版本查询是否正常'''
 
 
 import torch
-print(torch.cuda.is_available())  # 应返回True，如果CUDA可用的话
-print(torch.cuda.device_count())  # 显示可用的CUDA设备数量
 
-print(torch.cuda.device_count())
-import torch
-
-print("CUDA available:", torch.cuda.is_available())
+print('显示可用的CUDA设备数量:',torch.cuda.device_count())  # 显示可用的CUDA设备数量
+print("CUDA available（如果CUDA可用的话,应返回True:）:", torch.cuda.is_available())
+print('检查是否能调用cudnn，Ture表示可以调用：',torch.backends.cudnn.is_available())
 print("CUDA device count:", torch.cuda.device_count())
 print("CUDA device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "N/A")
-import torch
-print(torch.cuda.is_available())
-print(torch.backends.cudnn.is_available())
-print(torch.cuda_version)
-print(torch.backends.cudnn.version())
-
-import torch
-print(torch.__version__) #查阅pytorch版本
-
+print('cuda版本：',torch.cuda_version)
+print('cudnn版本:',torch.backends.cudnn.version())
+print('pytorch版本:',torch.__version__) #查阅pytorch版本
 import sys
-print(sys.version) #查询python版本
+print('python版本:',sys.version) #查询python版本
